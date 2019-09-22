2b4fa6dea48b84ea2cdbcf3c4eebd739ee83ff26
Adnan Hasani <adnanhasani@gmail.com>
2019-09-22 11:15:29 +0200

    Connecting to Mongo DB

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const chalk = require('chalk');
+const connectDB = require('./config/database');
 
 const app = express();
+
+connectDB();
 
 // Initializing Middleware
 app.use(express.json({ extended: false }));
