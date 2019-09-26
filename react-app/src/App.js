4de10157ee524715810c301f04756870591954a2
Adnan Hasani <adnanhasani@gmail.com>
2019-09-26 19:42:47 +0200

    Including Materialize CSS and Materialize JS to the App js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   return (
     <div className="App">
       <h1>Search Saver</h1>
