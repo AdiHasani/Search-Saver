389d0cf46bd1c6936991a236db2d6359eab998dd
Adnan Hasani <adnanhasani@gmail.com>
2019-09-27 04:41:03 +0200

    Exporting reducers

--- react-app/src/reducers/index.js
+++ react-app/src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+import searchReducer from './searchReducer';
 
-export default combineReducers({});
+export default combineReducers({
+  auth: authReducer,
+  search: searchReducer
+});
