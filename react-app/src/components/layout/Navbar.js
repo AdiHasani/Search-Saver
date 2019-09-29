257b5db73374a1553d38ce963116ea4ce5d10f03
Adnan Hasani <adnanhasani@gmail.com>
2019-09-29 16:23:06 +0200

    Now I pass user and logout action to navbar so we use that to show the user.name in navbar and also logout button for logout

--- react-app/src/components/layout/Navbar.js
+++ react-app/src/components/layout/Navbar.js
@@ -1,32 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/authActions';
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
+const Navbar = ({ user, logout }) => {
+  const onLogout = () => {
+    logout();
+  };
+
   return (
     <nav className="nav-extended blue">
       <div className="nav-wrapper container">
-        <a href="#!" className="brand-logo">
+        <a href="#" className="brand-logo">
           Search<span className="white blue-text r-p">Saver</span>
         </a>
         <ul className="right">
           <li>
-            <a
-              href="#!"
-              className="waves-effect waves-light btn blue modal-trigger"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#about-modal"
-              className="waves-effect waves-light btn blue modal-trigger"
-            >
+            <a href="#about-modal" className="modal-trigger">
               About
             </a>
           </li>
+          <li>Hello {user && user.name}</li>
           <li>
-            <a href="#!">
-              Logout <i className="fas fa-sign-out-alt"></i>
+            <a href="" onClick={onLogout}>
+              <i className="fas fa-sign-out-alt fa-sm" /> Logout
             </a>
           </li>
         </ul>
@@ -35,4 +32,15 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  user: state.auth.user
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
