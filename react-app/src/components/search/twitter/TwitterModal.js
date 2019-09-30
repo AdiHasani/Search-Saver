54d0386e08f45cc29db1ce80dc9d32eb9abdc670
Adnan Hasani <adnanhasani@gmail.com>
2019-09-30 14:23:29 +0200

    Added the functionality to search from frontend app, show last 10 quries with posibility to search when its clicked //TODO edit the query and update the query

--- react-app/src/components/search/twitter/TwitterModal.js
+++ react-app/src/components/search/twitter/TwitterModal.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import EditQueryModal from './EditQueryItem';
 import QueryItem from './QueryItem';
 import SelectOptions from './SelectOptions';
+import { searchTwitter, clearCurrent } from '../../../actions/searchActions';
 
-const TwitterModal = () => {
-  const querys = [];
-  let loading = true;
+const TwitterModal = ({
+  userData,
+  loading,
+  current,
+  searchTwitter,
+  clearCurrent
+}) => {
+  const [query, setQuery] = useState({
+    q: '',
+    result_type: 'popular',
+    count: 25
+  });
+  const { q, result_type, count } = query;
 
-  for (let i = 0; i < 15; i++) {
-    querys.push({ text: `Javascript ${i + 1}`, id: i + 1 });
-  }
-  const onSubmit = () => {
-    M.toast({ html: 'Hello world, part 2' });
+  useEffect(() => {
+    if (current !== null) {
+      setQuery(current);
+      searchTwitter(current);
+      clearCurrent();
+    }
+  }, [current]);
+
+  const onChange = e => setQuery({ ...query, [e.target.name]: e.target.value });
+  const onSubmit = e => {
+    e.stopPropagation();
+    if (q === '') {
+      M.toast({ html: 'Please enter a query.' });
+    } else {
+      searchTwitter(query);
+      setQuery({
+        q: '',
+        result_type: 'popular',
+        count: 25
+      });
+    }
   };
 
   return (
-    <div id="twitter-modal" className="modal" style={modalStyle}>
+    <div id="twitter-modal" className="modal">
       <div className="container">
         <div className="modal-head">
           <h4 className="center mb-2 mt-2">Search Twitter's tweets</h4>
@@ -24,13 +53,11 @@ const TwitterModal = () => {
             <div className="input-field">
               <input
                 type="text"
-                name="message"
-                value="{message}"
-                onChange={onSubmit}
+                name="q"
+                value={q}
+                placeholder="Standard tweet search"
+                onChange={onChange}
               />
-              <label htmlFor="message" className="active">
-                Search tweets
-              </label>
             </div>
           </div>
 
@@ -38,14 +65,11 @@ const TwitterModal = () => {
             <div className="col s8 m8 l10">
               <div className="input-field">
                 <select
-                  name="tech"
-                  value="{tech}"
+                  name="result_type"
+                  value={result_type}
                   className="browser-default"
-                  onChange={onSubmit}
+                  onChange={onChange}
                 >
-                  <option value="" disabled>
-                    Most
-                  </option>
                   <SelectOptions />
                 </select>
               </div>
@@ -55,9 +79,11 @@ const TwitterModal = () => {
                 <p>
                   <label>
                     <input
-                      type="text"
+                      type="number"
+                      name="count"
+                      value={count}
                       placeholder="1 to 100"
-                      onChange={onSubmit}
+                      onChange={onChange}
                     />
                   </label>
                 </p>
@@ -67,16 +93,23 @@ const TwitterModal = () => {
         </div>
         <div className="modal-body">
           <ul className="collection with-header">
-            <li className="collection-header">
-              <h5 className="center">Recently searched for</h5>
-            </li>
-            {!loading && querys.length === 0 ? (
-              <p className="center">No logs to show...</p>
+            {!loading && userData.queries.length === 0 ? (
+              <li className="collection-header">
+                <h5 className="center">No saved search queries</h5>
+              </li>
             ) : (
-              querys
-                .slice(0, 10)
-                .map(query => <QueryItem query={query} key={query.id} />)
+              <li className="collection-header">
+                <h5 className="center">Recently searched for:</h5>
+              </li>
             )}
+            {!loading &&
+              userData.queries.length !== 0 &&
+              userData.queries
+                .slice(Math.max(userData.queries.length - 10, 1))
+                .reverse()
+                .map(query => (
+                  <QueryItem query={query.query} key={query._id} />
+                ))}
           </ul>
         </div>
         <div className="modal-footer center">
@@ -93,9 +126,18 @@ const TwitterModal = () => {
     </div>
   );
 };
-const modalStyle = {
-  width: '75vw',
-  maxHeight: '75vh'
+
+TwitterModal.propTypes = {
+  userData: PropTypes.object.isRequired,
+  current: PropTypes.object
 };
 
-export default TwitterModal;
+const mapStateToProps = state => ({
+  userData: state.search.data,
+  current: state.search.current
+});
+
+export default connect(
+  mapStateToProps,
+  { searchTwitter, clearCurrent }
+)(TwitterModal);
